8dff3a5110eef9c0da9f1d65912c57054318905c
rosatiara <rosatiaragaluh@gmail.com>
2024-09-17 19:48:53 +0700

    Initial Commit

---
+++ AppleSingaporePresentations/AppleSingaporePresentationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSingaporePresentationsApp.swift
+//  AppleSingaporePresentations
+//
+//  Created by Rosa Tiara Galuh on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleSingaporePresentationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleSingaporePresentations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleSingaporePresentations
+//
+//  Created by Rosa Tiara Galuh on 17/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
