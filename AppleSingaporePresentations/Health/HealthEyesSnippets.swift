458da0fc877d458509c1f392ce7931d68880e4d5
Rosa Tiara Galuh <rosatiaragaluh@gmail.com>
2024-09-19 21:18:29 +0700

    Update HealthEyesSnippets.swift

--- AppleSingaporePresentations/Health/HealthEyesSnippets.swift
+++ AppleSingaporePresentations/Health/HealthEyesSnippets.swift
@@ -30,33 +30,12 @@ let sphere = HKQuantity(unit: .diopter(), doubleValue: -0.75)
 let cylinder = HKQuantity(unit: .diopter(), doubleValue: -0.5)
 let axis = HKQuantity(unit: .degreeAngle(), doubleValue: 155.0)
 let addPower = HKQuantity(unit: .diopter(), doubleValue: +2.00)
-let baseCurve = HKQuantity(unit: HKUnit.meterUnit(with: .milli),
-                           doubleValue: 9.0)
+let baseCurve = HKQuantity(unit: HKUnit.meterUnit(with: .milli), doubleValue: 9.0)
+let diameter = HKQuantity(unit: HKUnit.meterUnit(with: .milli), doubleValue: 12.0)
 
-
-let diameter = HKQuantity(unit: HKUnit.meterUnit(with: .milli),
-                          doubleValue: 12.0)
-
-let contactsRightEye = HKContactsLensSpecification(sphere: sphere,
-                                                   cylinder: cylinder,
-                                                   axis: axis,
-                                                   addPower: addPower,
-                                                   baseCurve: baseCurve,
-                                                   diameter: diameter)
-
-let contactsLeftEye = HKContactsLensSpecification(sphere: sphere,
-                                                   cylinder: cylinder,
-                                                   axis: axis,
-                                                   addPower: addPower,
-                                                   baseCurve: baseCurve,
-                                                   diameter: diameter)
+let contactsRightEye = HKContactsLensSpecification(sphere: sphere,cylinder: cylinder, axis: axis, addPower: addPower, baseCurve: baseCurve, diameter: diameter)
+let contactsLeftEye = HKContactsLensSpecification(sphere: sphere,cylinder: cylinder, axis: axis, addPower: addPower, baseCurve: baseCurve, diameter: diameter)
 
 let dateIssued = Date()
 let expirationDate = dateIssued.addingTimeInterval(60 * 24 * 365)
-let prescription = HKContactsPrescription(rightEyeSpecification: contactsRightEye,
-                                          leftEyeSpecification: contactsLeftEye,
-                                          brand: "MyBrand Name",
-                                          dateIssued: dateIssued,
-                                          expirationDate: expirationDate,
-                                          device: HKDevice.local(),
-                                          metadata: nil)
+let prescription = HKContactsPrescription(rightEyeSpecification: contactsRightEye, leftEyeSpecification: contactsLeftEye, brand: "Apple Eye Lens", dateIssued: dateIssued, expirationDate: expirationDate, device: HKDevice.local(), metadata: nil)
